4c184d4a9f88215dfa4f9e3371bf85fb63fa8975
Tavkel <gmininb@gmail.com>
2023-08-07 22:29:39 +0300

    Initial commit

---
+++ src/main/java/com/example/Homework_230806/Homework230806Application.java
@@ -0,0 +1,13 @@
+package com.example.Homework_230806;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework230806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework230806Application.class, args);
+	}
+
+}
